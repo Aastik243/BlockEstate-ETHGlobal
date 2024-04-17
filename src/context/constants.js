53309e2866f6853b07621e5d999264b7874c19c5
Aastik <aastik243@gmail.com>
2024-04-17 18:18:41 +0530

    added renttoown context api

--- src/context/constants.js
+++ src/context/constants.js
@@ -1,6 +1,9 @@
 import propertyRegistry from '../../contracts/out/propertyRegistry.sol/PropertyRegistry.json'
 import Purchasing from '../../contracts/out/Purchasing.sol/Purchasing.json'
 
+
+export const PurchasingAddress = 0xedD8b8f58f899d214Cf23417bc982E1dA2C2AC86;
+export const propertyRegistryAddress = 0x77dae173a6C655D37Ed5bf59eC1ea6717279530e;
 export const propertyRegistryABI = propertyRegistry.abi;
 export const PurchasingABI = Purchasing.abi;
 
